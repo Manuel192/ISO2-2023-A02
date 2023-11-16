287fe790d21d35f68354bd9764786c4f0e596ec0
hnevesg <helena.neves@alu.uclm.es>
2023-11-16 13:51:30 +0100

    Plantilla Maven
    Maven project created based on a template

---
+++ ISO 2 Lab/src/test/java/A1_05/project_name/AppTest.java
@@ -0,0 +1,20 @@
+package A1_05.project_name;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
