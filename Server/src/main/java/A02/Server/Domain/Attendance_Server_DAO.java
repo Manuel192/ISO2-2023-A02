c172ac2b0e6f2fac32323e0e35a7b1f96eba1549
Lucia-Martin17 <Lucia.Martin17@alu.uclm.es>
2023-11-27 19:52:33 +0100

    Server branch
    Reorganize the code corresponding to the server system from iterations 1 and 2 according to the changes planned for 27th of November

---
+++ Server/src/main/java/A02/Server/Domain/Attendance_Server_DAO.java
@@ -0,0 +1,27 @@
+package It2-CollectAttendance.Domain_server;
+
+import It2-CollectAttendance.*;
+import It2-CollectAttendance.Domain_client.*;
+
+public class Attendance_Server_DAO {
+
+	/**
+	 * 
+	 * @param event
+	 * @param localization
+	 */
+	public void add(Event event, float localization) {
+		// TODO - implement Attendance_Server_DAO.add
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param collectID
+	 */
+	public Attendance read(int collectID) {
+		// TODO - implement Attendance_Server_DAO.read
+		throw new UnsupportedOperationException();
+	}
+
+}
