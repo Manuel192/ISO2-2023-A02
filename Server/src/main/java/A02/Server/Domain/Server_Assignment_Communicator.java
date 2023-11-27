c55852d2d4b85cfbe143b30486c108ca45bfe612
hnevesg <helena.neves@alu.uclm.es>
2023-11-28 00:22:52 +0100

    It3-Feature

---
+++ Server/src/main/java/A02/Server/Domain/Server_Assignment_Communicator.java
@@ -0,0 +1,12 @@
+package Domain_Server;
+
+public class Server_Assignment_Communicator {
+
+	public void treat(String aRequest) {
+		throw new UnsupportedOperationException();
+	}
+
+	public void send(String aReply) {
+		throw new UnsupportedOperationException();
+	}
+}
