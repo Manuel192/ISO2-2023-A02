4ea2b38d34ed0406cee83258a94363e6743da73b
Lucia-Martin17 <Lucia.Martin17@alu.uclm.es>
2023-11-27 22:33:05 +0100

    Organizer

---
+++ Organizer/src/main/java/A02/Organizer/Domain/Client_Attendance_Communicator.java
@@ -0,0 +1,14 @@
+package It2-CollectAttendance.Domain_client;
+
+public class Client_Attendance_Communicator {
+
+	/**
+	 * 
+	 * @param request
+	 */
+	public void send(String request) {
+		// TODO - implement Client_Attendance_Communicator.send
+		throw new UnsupportedOperationException();
+	}
+
+}
