55f88bc79232b4a068ba493b7b5e5360b3d706ee
hnevesg <helena.neves@alu.uclm.es>
2023-11-28 00:27:00 +0100

    MAN-It3-feature

---
+++ Manager/src/main/java/Domain_client/Assignment.java
@@ -0,0 +1,17 @@
+package Domain_Client;
+
+public class Assignment {
+	private int _assignmentID;
+	private String _memberID;
+	private String _courseID;
+	public Course _unnamed_Course_;
+	public Member _unnamed_Member_;
+
+	public Assignment(int aAssID, String aMember, int aCourse) {
+		throw new UnsupportedOperationException();
+	}
+
+	public String toString() {
+		throw new UnsupportedOperationException();
+	}
+}
