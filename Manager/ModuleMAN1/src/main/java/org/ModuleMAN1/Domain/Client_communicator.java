a414da40f8516fe4bf479d81a40759b9a9a9150a
Lucia-Martin17 <Lucia.Martin17@alu.uclm.es>
2023-12-14 21:25:47 +0100

    Corrections System v.0.1.3 M.MAN.1 v.0.1.3

---
+++ Manager/ModuleMAN1/src/main/java/org/ModuleMAN1/Domain/Client_communicator.java
@@ -0,0 +1,38 @@
+package org.ModuleMAN1.Domain;
+
+/** 
+ * <p>
+ *         <b>Class:</b></br>
+ *         Client_communicator.
+ * </p>
+ * 
+ * <p>
+ *         <b>Description:</b></br>
+ *         Class to communicate with the server.
+ * </p>
+ * 
+ * <p>
+ *         <b>Control change:</b></br>
+ *         <ul>
+ *             <li> <b>0.1.0:</b> Create class.
+ *             <li> <b>0.1.1:</b> Add Javadoc.
+ *             <li> <b>0.1.2:</b> Add it again to the module.
+ *         </ul>
+ * </p>
+ * 
+ * @author A02-Group-C
+ * @since 0.1.0
+ * @version 0.1.2
+ */
+public class Client_communicator {
+
+	/**
+	 * 
+	 * @param request
+	 */
+	public void send(String request) {
+		// TODO - implement Client_Communicator.send
+		throw new UnsupportedOperationException();
+	}
+
+}
